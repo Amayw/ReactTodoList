6f84a2dc75b79be836e0090aebd63804f976d2e5
wywang <727936199@qq.com>
2021-05-31 11:50:21 +0800

    安装redux，创建store和reducer

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,33 +1,31 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import {Button,Input,List,Typography} from 'antd';
-
+import store from './store';
 
 const WrapperStyle={
     display:'flex',
     flexDirection:'column',
-    alignItems:'center'
+    alignItems:'center',
 }
 
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
-class TodoList extends Component {
 
-    render() { 
-        return ( 
+class TodoList extends Component {
+    constructor(props){
+        super(props);
+        this.state=store.getState();
+    }
+    render() {
+        return (
             <div style={WrapperStyle}>
                 <header>
-                    <Input placeholder='input' style={{width:'250px',margin:'20px 10px 20px 0'}}/>
+                    <Input placeholder={this.state.inputValue} style={{width:'250px',margin:'20px 10px 20px 0'}}/>
                     <Button type='primary'>Add</Button>
                 </header>
                 <main>
                 <List
-                    dataSource={data}
+                    style={{paddingBottom:'50px'}}
+                    dataSource={this.state.list}
                     renderItem={item => (
                         <List.Item>
                         <Typography.Text mark>[ITEM]</Typography.Text> {item}
@@ -36,8 +34,8 @@ class TodoList extends Component {
                 />
                 </main>
             </div>
-         );
+         )
     }
 }
- 
+
 export default TodoList;
